7a25b4c5798b7bab76166f926ddda5750781ab8a
kiran <gandikotakiran99@gmail.com>
2022-01-12 10:20:26 +0530

    extracting data from api

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react';
+import { Container,Row,Col } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import './App.css';
+
+import Axios from 'axios';
+
+function App() {
+  const [details,setDetails] = useState({});
+
+  const fetchDetails = async ()=>{
+    const response = await Axios.get("https://randomuser.me/api/");
+    console.log("RESPONSE :" , response)
+  }
+
+  useEffect(()=>{
+    fetchDetails();
+  },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
